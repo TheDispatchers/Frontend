123c960305dec2e7004477e7aa0d041e436004d0
Matej Mraz <matej.misura@gmail.com>
2017-06-02 23:30:18 +0200

    Added confirmPage.. Added comments to Ride Page and reorganized the whole code into methods and so on..

--- iTaxApp/iTaxApp/iTaxApp.Android/RidePage.xaml.cs
+++ iTaxApp/iTaxApp/iTaxApp.Android/RidePage.xaml.cs
@@ -1,6 +1,6 @@
-﻿using Plugin.Geolocator;
+﻿using iTaxApp.Droid;
+using Plugin.Geolocator;
 using System;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
@@ -10,48 +10,68 @@ namespace iTaxApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RidePage : ContentPage
     {
+        /* VARIABLES DECLARATION */
         Geocoder geoCoder;
         Pin pin;
         string fromLatitude;
         string fromLongitude;
         string toLatitude;
         string toLongitude;
+
+        /* CONSTRUCTOR */
         public RidePage()
         {
             InitializeComponent();
             geoCoder = new Geocoder();
-            customMap.RouteCoordinates.Add(new Position(37.785559, -122.396728));
-            customMap.RouteCoordinates.Add(new Position(37.780624, -122.390541));
-            customMap.RouteCoordinates.Add(new Position(37.777113, -122.394983));
-            customMap.RouteCoordinates.Add(new Position(37.776831, -122.394627));
-            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(37.79752, -122.40183), Distance.FromMiles(1.0)));
         }
-        async void OnRefresh(object sender, EventArgs e)
-        {
-            reverseGeocodedOutputLabel.Text = "Searching..";
-            MyMap.Pins.Clear();
-            var locator = CrossGeolocator.Current;
-            var pos = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
 
-            latitude.Text = "Latitude: " + pos.Latitude;
-            longitude.Text = "Longitude: " + pos.Longitude;
-            fromLatitude = pos.Latitude.ToString();
-            fromLongitude = pos.Longitude.ToString();
-            var position = new Position(pos.Latitude, pos.Longitude);
-            pin = new Pin
+        /// <summary>
+        /// Method that runs when the "Find my location" button is pressed. 
+        /// It finds the current position of the device and uses other method to translate it to an address.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void OnFind(object sender, EventArgs e)
+        {
+            MyMap.Pins.Clear(); //Clear all previous pins that were on the map.
+
+            var locator = CrossGeolocator.Current; // Create the geo locator
+            var pos = await locator.GetPositionAsync(timeoutMilliseconds: 10000); // Get the current position from the device's GPS HW
+            var position = new Position(pos.Latitude, pos.Longitude); // Instantiate the Position with the data obtained from the device
+
+            reverseGeocodedOutputLabel.Text = "Searching..";  // Verification for user, that the device is searching for location.
+
+            latitude.Text = "Latitude: " + pos.Latitude; // Update label.
+            longitude.Text = "Longitude: " + pos.Longitude; // Update label.
+
+            fromLatitude = pos.Latitude.ToString(); // Store the value for later access.
+            fromLongitude = pos.Longitude.ToString(); // Store the value for later access.
+
+            pin = new Pin /* Create new Pin on the map - Start location */
             {
                 Type = PinType.Place,
                 Position = position,
                 Label = "Start location",
                 Address = "Your taxi will pick you up here."
             };
-            MyMap.Pins.Add(pin);
-            customMap.RouteCoordinates.Add(new Position(37.785559, -122.396728));
-            customMap.RouteCoordinates.Add(new Position(37.780624, -122.390541));
-            customMap.RouteCoordinates.Add(new Position(37.777113, -122.394983));
-            customMap.RouteCoordinates.Add(new Position(37.776831, -122.394627));
-            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(37.79752, -122.40183), Distance.FromMiles(1.0)));
+            MyMap.Pins.Add(pin); // Add the pin to the map.
 
+            DecodeAddress(position); // Decode the GPS Coordinates into an address *** More info in method description ***
+
+            if (destination.Text != null) //Prevent trying to decode address to GPS coordinates if the address field is empty.
+            {
+                DecodeCoords(); // Decode the address to GPS Coordinates *** More info in method description ***
+            }
+        }
+
+        /// <summary>
+        /// This method uses the GeoCoder plugin to get the most probable address for the GPS position provided to it. 
+        /// The GeoCoder works asynchronously so the method can't actually return a value, therefore it updates the relevant label instead.
+        /// As this function by default returns multiple possible addresses, counter is attached to it to limit the output to only one address.
+        /// </summary>
+        /// <param name="position"></param>
+        public async void DecodeAddress(Position position)
+        {
             string[] myAddress = new string[3];
             {
                 var possibleAddresses = await geoCoder.GetAddressesForPositionAsync(position);
@@ -67,56 +87,54 @@ namespace iTaxApp
                     }
                 }
             }
-            if (destination.Text != null)
+        }
+        /// <summary>
+        /// This method uses the GeoCoder plugin to get the GPS position for the address provided. 
+        /// Then it places a pin on the map with the destination and some information for the user.
+        /// It interacts with the UI directly.
+        /// </summary>
+        public async void DecodeCoords()
+        {
+            var addressToCode = destination.Text;
+            var approximateLocations = await geoCoder.GetPositionsForAddressAsync(addressToCode);
+            foreach (var destinationpos in approximateLocations)
             {
-                var addressToCode = destination.Text;
-                var approximateLocations = await geoCoder.GetPositionsForAddressAsync(addressToCode);
-                foreach (var destinationpos in approximateLocations)
+                geocodedOutputLabel.Text = destinationpos.Latitude + ", " + destinationpos.Longitude + "\n";
+                toLatitude = destinationpos.Latitude.ToString();
+                toLongitude = destinationpos.Longitude.ToString();
+                pin = new Pin
                 {
-                    geocodedOutputLabel.Text = destinationpos.Latitude + ", " + destinationpos.Longitude + "\n";
-                    toLatitude = destinationpos.Latitude.ToString();
-                    toLongitude = destinationpos.Longitude.ToString();
-                    pin = new Pin
-                    {
-                        Type = PinType.Place,
-                        Position = destinationpos,
-                        Label = "Destination",
-                        Address = "Your taxi will drop you off here."
-                    };
-                    MyMap.Pins.Add(pin);
-                }
+                    Type = PinType.Place,
+                    Position = destinationpos,
+                    Label = "Destination",
+                    Address = "Your taxi will drop you off here."
+                };
+                MyMap.Pins.Add(pin);
             }
         }
+
+        /// <summary>
+        /// This method opens the ExtrasPage for user to choose extras from.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void OnExtras(object sender, EventArgs e)
         {
             Navigation.PushAsync(new ExtrasPage());
         }
+
+        /// <summary>
+        /// This method gathers all the necessary data to make an order of a taxi and directs the user to the confirm page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         void OnOrder(object sender, EventArgs e)
         {
             Ride ride;
             string sessionKey = Convert.ToString(App.Current.Properties["sessionKey"]);
-            Console.WriteLine("Ses key: " + sessionKey);
             ride = new Ride(fromLatitude, fromLongitude, toLatitude, toLongitude, sessionKey);
             object obj = SynchronousSocketClient.StartClient("oderRide", ride);
-            Navigation.PushAsync(new ExtrasPage());
+            Navigation.PushAsync(new ConfirmPage());
         }
-
     }
 }
-
-/*
-           var position = new Position(54.9134468, 9.7827599); // Latitude, Longitude
-           var pin = new Pin
-           {
-               Type = PinType.Place,
-               Position = position,
-               Label = "Destination",
-               Address = "Order a taxi to this location."
-           };
-           MyMap.Pins.Add(pin);
-           /*customMap.RouteCoordinates.Add(new Position(37.785559, -122.396728));
-           customMap.RouteCoordinates.Add(new Position(37.780624, -122.390541));
-           customMap.RouteCoordinates.Add(new Position(37.777113, -122.394983));
-           customMap.RouteCoordinates.Add(new Position(37.776831, -122.394627));
-           customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(37.79752, -122.40183), Distance.FromMiles(1.0)));
-           */
